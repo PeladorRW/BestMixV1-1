3487b4aa4433814bac3abe9140f56dfc13fa4eeb
Conrad Brock <conrad_brock@hotmail.com>
2020-05-21 19:53:23 +0100

    MP Compat

---
+++ BestMix/MultiplayerSupport.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Verse;
+
+namespace BestMix
+{
+    [StaticConstructorOnStartup]
+    static class MultiplayerSupport
+    {
+        static Harmony harmony = new Harmony("rimworld.pelador.bestmix.multiplayersupport");
+
+        static MultiplayerSupport()
+        {
+            if (!MP.enabled) return;
+
+            //SyncMethods
+            MP.RegisterSyncMethod(typeof(BMBillUtility), nameof(BMBillUtility.SetBMixBillMode));
+            MP.RegisterSyncMethod(typeof(CompBestMix), nameof(CompBestMix.SetBMixMode));
+
+            // Add all Methods where there is Rand calls here
+            var methods = new[] {
+            AccessTools.Method(typeof(BestMixUtility), nameof(BestMixUtility.RNDFloat))
+        };
+            foreach (var method in methods)
+            {
+                FixRNG(method);
+            }
+        }
+
+        static void FixRNG(MethodInfo method)
+        {
+            harmony.Patch(method,
+                prefix: new HarmonyMethod(typeof(MultiplayerSupport), nameof(FixRNGPre)),
+                postfix: new HarmonyMethod(typeof(MultiplayerSupport), nameof(FixRNGPos))
+            );
+        }
+
+        static void FixRNGPre() => Rand.PushState(Find.TickManager.TicksAbs);
+        static void FixRNGPos() => Rand.PopState();
+    }
+}
